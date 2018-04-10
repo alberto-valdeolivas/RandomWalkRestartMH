e6ec0626e2ee18f363394b1b7f8c23cc88786fd9
Alberto Valdeolivas <alvaldeolivas@gmail.com>
2018-04-10 10:48:03 +0200

    Add files via upload

---
+++ tests/testthat/test_objectstype.R
@@ -0,0 +1,38 @@
+library(RandomWalkRestartMH)
+
+############################################################
+context("Objects Comprobation")
+############################################################
+
+## Multiplex
+m1 <- igraph::graph(c(1,2,1,3,2,3), directed = FALSE)
+m2 <- igraph::graph(c(1,3,2,3,3,4,1,4), directed = FALSE)
+multiObject <- create.multiplex(m1,m2)
+AdjMatrix <- compute.adjacency.matrix(multiObject)
+AdjMatrixNorm <- normalize.multiplex.adjacency(AdjMatrix)
+Multiplex_Seeds <- c(1)
+
+RWR_MultiResults <- Random.Walk.Restart.Multiplex(AdjMatrixNorm, multiObject,
+    Multiplex_Seeds)
+
+
+## Multiplex-Heterogeneous
+h1 <- igraph::graph(c("A","C","B","E","E","D","E","C"), directed = FALSE)
+bipartite_relations <- data.frame(m=c(1,3),h=c("A","E"))
+
+multiHetObject <- create.multiplexHet(multiObject,
+    h1,bipartite_relations)
+MultiHetTranMatrix <- compute.transition.matrix(multiHetObject)
+
+SecondNet_Seeds <- c("E")
+
+RWR_MultiHetResults <- Random.Walk.Restart.MultiplexHet(MultiHetTranMatrix,
+    multiHetObject,Multiplex_Seeds,SecondNet_Seeds)
+
+test_that("Objects are of the expected type", {
+    expect_equal(is.multiplex(multiObject), TRUE)
+    expect_equal(is.multiplex.het(multiHetObject), TRUE)
+    expect_equal(is.RWRM.Results(RWR_MultiResults), TRUE)
+    expect_equal(is.RWRMH.Results(RWR_MultiHetResults), TRUE)
+})
+
